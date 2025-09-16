504d3d96d3d53d65b23b3f9db4e68951f1299f37
Michael Larbi <mickylarbi28@gmail.com>
2025-09-16 02:20:12 +0000

    fix: enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
-  })
+    origin: '*', // Allows all domains to access the resources (use carefully in production)
+    methods: 'GET,PATCH,POST,DELETE', // Allowed HTTP methods
+    allowedHeaders: 'Content-Type, Accept, Authorization, X-App-Identifier', // Allowed headers
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
