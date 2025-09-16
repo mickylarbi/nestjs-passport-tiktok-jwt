72951c0d1f7945721c2c8b56af7a3b9688bde296
Michael Larbi <mickylarbi28@gmail.com>
2025-09-16 02:32:20 +0000

    fix: import config module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    JwtModule.register({ global: true }),
+    ConfigModule.forRoot({ isGlobal: true }),
     AuthModule
   ],
   controllers: [AppController],
